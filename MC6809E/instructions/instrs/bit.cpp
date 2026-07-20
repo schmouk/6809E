6ff01dcc0ddb974a4a5f32d84e40ad9243ad96bd
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-07-20 22:42:11 +0200

    #15-implement the M6809 CPU instructions set
    Added BIT instruction.

---
+++ MC6809E/instructions/instrs/bit.cpp
@@ -0,0 +1,221 @@
+#include <cstdint>
+
+#include "./bit.h"
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../addressing_modes/indexed_addressing_factory.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+#include "../../cpu/microproc_unit.h"
+#include "../../cpu/cpu_registers.h"
+
+
+namespace instr
+{
+    //=====   BIT Base Class   ================================
+    //---------------------------------------------------------
+    BITBase::BITBase(archi::HWArchitecture& hw_arch, const memory::Byte opcode) noexcept
+        : BaseInstruction(hw_arch, opcode)
+    {}
+
+    //---------------------------------------------------------
+    const memory::Byte BITBase::_evaluate(const int reg_value, const int mem_value)
+    {
+        const int intermediate_value{ _evaluate_value(reg_value, mem_value) };
+        _evaluate_cc_flags(intermediate_value);
+        return memory::Byte(intermediate_value & 0xff);
+    }
+
+    //---------------------------------------------------------
+    void BITBase::_evaluate_cc_flags(const int intermediate_value)
+    {
+        _hw_arch.regCC.set_negative((intermediate_value & 0x80) != 0);
+        _hw_arch.regCC.set_zero(intermediate_value != 0);
+        _hw_arch.regCC.clr_overflow();
+    }
+
+    //---------------------------------------------------------
+    const int BITBase::_evaluate_value(const int reg_value, const int mem_value)
+    {
+        return reg_value & mem_value;
+    }
+
+
+    //=====   BITA   ==========================================
+    //-----   BITA Immediate   --------------------------------
+    //---------------------------------------------------------
+    BITAImmediate::BITAImmediate(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0x89 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITAImmediate::exec()
+    {
+        memory::Byte mem_value{ _hw_arch.load_next_byte() };
+        _hw_arch.regA = _evaluate(_hw_arch.regA, mem_value);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITAImmediate::get_cycles_count() noexcept
+    {
+        return 2;
+    }
+
+
+    //-----   BITA Direct   -----------------------------------
+    //---------------------------------------------------------
+    BITADirect::BITADirect(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0x99 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITADirect::exec()
+    {
+        memory::Byte value_low_addr{ _hw_arch.load_next_byte() };
+        memory::Byte mem_value{ _hw_arch.get_byte(_hw_arch.get_directpage_addr(value_low_addr)) };
+
+        _hw_arch.regA = _evaluate(_hw_arch.regA, mem_value);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITADirect::get_cycles_count() noexcept
+    {
+        return 4;
+    }
+
+
+    //-----   BITA Indexed   ----------------------------------
+    //---------------------------------------------------------
+    BITAIndexed::BITAIndexed(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0xA9 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITAIndexed::exec()
+    {
+        _indexed_mode_ptr = addr::make_indexed_addressing_class(_hw_arch);
+        _hw_arch.regA = _evaluate(_hw_arch.regA, _indexed_mode_ptr->get_addressed_byte());
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITAIndexed::get_cycles_count() noexcept
+    {
+        return 4 + _indexed_mode_ptr->get_byte_cycles();
+    }
+
+
+    //-----   BITA Extended   ---------------------------------
+    //---------------------------------------------------------
+    BITAExtended::BITAExtended(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0xB9 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITAExtended::exec()
+    {
+        memory::MemAddr value_addr{ memory::MemAddr(_hw_arch.load_next_word()) };
+        memory::Byte mem_value{ _hw_arch.get_byte(value_addr) };
+
+        _hw_arch.regA = _evaluate(_hw_arch.regA, mem_value);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITAExtended::get_cycles_count() noexcept
+    {
+        return 5;
+    }
+
+
+    //=====   BITB   ==========================================
+    //-----   BITB Immediate   --------------------------------
+    //---------------------------------------------------------
+    BITBImmediate::BITBImmediate(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0xC9 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITBImmediate::exec()
+    {
+        memory::Byte mem_value{ _hw_arch.load_next_byte() };
+        _hw_arch.regB = _evaluate(_hw_arch.regB, mem_value);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITBImmediate::get_cycles_count() noexcept
+    {
+        return 2;
+    }
+
+
+    //-----   BITB Direct   -----------------------------------
+    //---------------------------------------------------------
+    BITBDirect::BITBDirect(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0xD9 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITBDirect::exec()
+    {
+        memory::Byte value_low_addr{ _hw_arch.load_next_byte() };
+        memory::Byte mem_value{ _hw_arch.get_byte(_hw_arch.get_directpage_addr(value_low_addr)) };
+
+        _hw_arch.regB = _evaluate(_hw_arch.regB, mem_value);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITBDirect::get_cycles_count() noexcept
+    {
+        return 4;
+    }
+
+    //-----   BITB Indexed   ----------------------------------
+    //---------------------------------------------------------
+    BITBIndexed::BITBIndexed(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0xE9 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITBIndexed::exec()
+    {
+        _indexed_mode_ptr = addr::make_indexed_addressing_class(_hw_arch);
+        _hw_arch.regB = _evaluate(_hw_arch.regB, _indexed_mode_ptr->get_addressed_byte());
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITBIndexed::get_cycles_count() noexcept
+    {
+        return 4 + _indexed_mode_ptr->get_byte_cycles();
+    }
+
+
+    //-----   BITB Extended   ---------------------------------
+    //---------------------------------------------------------
+    BITBExtended::BITBExtended(archi::HWArchitecture& hw_arch) noexcept
+        : BITBase{ hw_arch, 0xF9 }
+    {
+    }
+
+    //---------------------------------------------------------
+    void BITBExtended::exec()
+    {
+        memory::MemAddr value_addr{ memory::MemAddr(_hw_arch.load_next_word()) };
+        memory::Byte mem_value{ _hw_arch.get_byte(value_addr) };
+
+        _hw_arch.regB = _evaluate(_hw_arch.regB, mem_value);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BITBExtended::get_cycles_count() noexcept
+    {
+        return 5;
+    }
+
+}
---
+++ MC6809E/instructions/instrs/bit.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+
+
+namespace instr
+{
+    //=====   BIT Base Class   ================================
+    class BITBase : public BaseInstruction
+    {
+    public:
+        BITBase(archi::HWArchitecture& hw_arch, const memory::Byte opcode) noexcept;
+        virtual ~BITBase() noexcept = default;
+
+    protected:
+        const memory::Byte _evaluate(const int reg_value, const int mem_value);
+        virtual void       _evaluate_cc_flags(const int intermediate_value);
+        virtual const int  _evaluate_value(const int reg_value, const int mem_value);
+    };
+
+
+    //=====   BITA   ==========================================
+    //-----   BITA Immediate   --------------------------------
+    struct BITAImmediate : public BITBase
+    {
+        BITAImmediate(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITAImmediate() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   BITA Direct   -----------------------------------
+    struct BITADirect : public BITBase
+    {
+        BITADirect(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITADirect() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   BITA Indexed   ----------------------------------
+    class BITAIndexed : public BITBase
+    {
+    public:
+        BITAIndexed(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITAIndexed() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+
+    private:
+        std::unique_ptr<addr::BaseAddressingMode> _indexed_mode_ptr{ nullptr };
+    };
+
+    //-----   BITA Extended   ---------------------------------
+    struct BITAExtended : public BITBase
+    {
+        BITAExtended(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITAExtended() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+
+    //=====   BITB   ==========================================
+    //-----   BITB Immediate   --------------------------------
+    struct BITBImmediate : public BITBase
+    {
+        BITBImmediate(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITBImmediate() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   BITB Direct   -----------------------------------
+    struct BITBDirect : public BITBase
+    {
+        BITBDirect(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITBDirect() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   BITB Indexed   ----------------------------------
+    class BITBIndexed : public BITBase
+    {
+    public:
+        BITBIndexed(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITBIndexed() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+
+    private:
+        std::unique_ptr<addr::BaseAddressingMode> _indexed_mode_ptr{ nullptr };
+    };
+
+    //-----   BITB Extended   ---------------------------------
+    struct BITBExtended : public BITBase
+    {
+        BITBExtended(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~BITBExtended() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+}
