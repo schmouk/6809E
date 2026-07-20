546fb22b9788ca6a2b5f4777319fec126fb3dc52
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-07-20 22:35:52 +0200

    #15-implement the M6809 CPU instructions set
    Added ASR instruction.

---
+++ MC6809E/instructions/instrs/asr.cpp
@@ -0,0 +1,153 @@
+#include <cstdint>
+
+#include "./asr.h"
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../addressing_modes/indexed_addressing_factory.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+#include "../../cpu/microproc_unit.h"
+#include "../../cpu/cpu_registers.h"
+
+
+namespace instr
+{
+    //=====   ASR Base Class   ================================
+    //---------------------------------------------------------
+    ASRBase::ASRBase(archi::HWArchitecture& hw_arch, const memory::Byte opcode) noexcept
+        : BaseInstruction(hw_arch, opcode)
+    {}
+
+    //---------------------------------------------------------
+    const memory::Byte ASRBase::_evaluate(const int value)
+    {
+        const int intermediate_value{ _evaluate_value(value) };
+        _evaluate_cc_flags(intermediate_value);
+        return memory::Byte(intermediate_value & 0xff);
+    }
+
+    //---------------------------------------------------------
+    void ASRBase::_evaluate_cc_flags(const int intermediate_value)
+    {
+        _hw_arch.regCC.set_zero((intermediate_value & 0xff) == 0);
+        _hw_arch.regCC.set_negative((intermediate_value & 0x80) != 0);
+    }
+
+    //---------------------------------------------------------
+    const int ASRBase::_evaluate_value(const int value)
+    {
+        _hw_arch.regCC.set_carry((value & 0x01) != 0);
+
+        const int high_bit{ value & 0x80 };
+        return high_bit | (value >> 1);
+    }
+
+
+    //=====   ASRA   ==========================================
+    //-----   ASRA Inherent   ---------------------------------
+    ASRAInherent::ASRAInherent(archi::HWArchitecture& hw_arch) noexcept
+        : ASRBase{ hw_arch, 0x47 }
+    {}
+
+    //---------------------------------------------------------
+    void ASRAInherent::exec()
+    {
+        _hw_arch.regA = _evaluate(_hw_arch.regA);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t ASRAInherent::get_cycles_count() noexcept
+    {
+        return 2;
+    }
+
+
+    //=====   ASRB   ==========================================
+    //-----   ASRB Inherent   ---------------------------------
+    ASRBInherent::ASRBInherent(archi::HWArchitecture& hw_arch) noexcept
+        : ASRBase{ hw_arch, 0x57 }
+    {}
+
+    //---------------------------------------------------------
+    void ASRBInherent::exec()
+    {
+        _hw_arch.regB = _evaluate(_hw_arch.regB);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t ASRBInherent::get_cycles_count() noexcept
+    {
+        return 2;
+    }
+
+
+    //=====   ASR Memory   ====================================
+    //-----   ASR Direct   ------------------------------------
+    //---------------------------------------------------------
+    ASRDirect::ASRDirect(archi::HWArchitecture& hw_arch) noexcept
+        : ASRBase{ hw_arch, 0x07 }
+    {}
+
+    //---------------------------------------------------------
+    void ASRDirect::exec()
+    {
+        const memory::Byte value_low_addr{ _hw_arch.load_next_byte() };
+        const memory::MemAddr mem_addr{ _hw_arch.get_directpage_addr(value_low_addr) };
+        const memory::Byte mem_value{ _hw_arch.get_byte(mem_addr) };
+
+        _hw_arch.set_byte(mem_addr, _evaluate(mem_value));
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t ASRDirect::get_cycles_count() noexcept
+    {
+        return 6;
+    }
+
+
+    //-----   ASR Indexed   -----------------------------------
+    //---------------------------------------------------------
+    ASRIndexed::ASRIndexed(archi::HWArchitecture& hw_arch) noexcept
+        : ASRBase{ hw_arch, 0x67 }
+    {}
+
+    //---------------------------------------------------------
+    void ASRIndexed::exec()
+    {
+        _indexed_mode_ptr = addr::make_indexed_addressing_class(_hw_arch);
+
+        _indexed_mode_ptr->set_addressed_byte(
+            _evaluate(_indexed_mode_ptr->get_addressed_byte())
+        );
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t ASRIndexed::get_cycles_count() noexcept
+    {
+        return 6 + _indexed_mode_ptr->get_byte_cycles();
+    }
+
+
+    //-----   ASR Extended   ----------------------------------
+    //---------------------------------------------------------
+    ASRExtended::ASRExtended(archi::HWArchitecture& hw_arch) noexcept
+        : ASRBase{ hw_arch, 0x77 }
+    {}
+
+    //---------------------------------------------------------
+    void ASRExtended::exec()
+    {
+        memory::MemAddr mem_addr{ memory::MemAddr(_hw_arch.load_next_word()) };
+        memory::Byte mem_value{ _hw_arch.get_byte(mem_addr) };
+
+        _hw_arch.set_byte(mem_addr, _evaluate(mem_value));
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t ASRExtended::get_cycles_count() noexcept
+    {
+        return 7;
+    }
+
+}
---
+++ MC6809E/instructions/instrs/asr.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+
+
+namespace instr
+{
+    //=====   ASR Base Class   ================================
+    class ASRBase : public BaseInstruction
+    {
+    public:
+        ASRBase(archi::HWArchitecture& hw_arch, const memory::Byte opcode) noexcept;
+        virtual ~ASRBase() noexcept = default;
+
+    protected:
+        const memory::Byte _evaluate(const int value);
+        virtual void       _evaluate_cc_flags(const int intermediate_value);
+        virtual const int  _evaluate_value(const int value);
+    };
+
+
+    //=====   ASRA   ==========================================
+    //-----   ASRA Inherent   ---------------------------------
+    struct ASRAInherent : public ASRBase
+    {
+        ASRAInherent(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~ASRAInherent() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+
+    //=====   ASRB   ==========================================
+    //-----   ASRB Inherent   ---------------------------------
+    struct ASRBInherent : public ASRBase
+    {
+        ASRBInherent(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~ASRBInherent() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+
+    //=====   ASR Memory   ====================================
+    //-----   ASR Direct   ------------------------------------
+    struct ASRDirect : public ASRBase
+    {
+        ASRDirect(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~ASRDirect() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   ASR Indexed   -----------------------------------
+    class ASRIndexed : public ASRBase
+    {
+    public:
+        ASRIndexed(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~ASRIndexed() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+
+    private:
+        std::unique_ptr<addr::BaseAddressingMode> _indexed_mode_ptr{ nullptr };
+    };
+
+    //-----   ASR Extended   ----------------------------------
+    struct ASRExtended : public ASRBase
+    {
+        ASRExtended(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~ASRExtended() noexcept = default;
+
+        virtual void exec() override;
+        virtual const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+}
