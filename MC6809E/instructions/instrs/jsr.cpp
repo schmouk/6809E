238e248a6dccc005ff01944e02228e63ffd6ac62
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-07-23 13:46:41 +0200

    #15-implement the M6809 CPU instructions set
    Added JSR isntructions. Builds.

---
+++ MC6809E/instructions/instrs/jsr.cpp
@@ -0,0 +1,90 @@
+#include <cstdint>
+
+#include "./jsr.h"
+
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../addressing_modes/indexed_addressing_factory.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+#include "../../cpu/microproc_unit.h"
+#include "../../cpu/cpu_registers.h"
+
+
+namespace instr
+{
+    //=====   JSR Memory   ====================================
+    //-----   JSR Direct   ------------------------------------
+    //---------------------------------------------------------
+    JSRDirect::JSRDirect(archi::HWArchitecture& hw_arch) noexcept
+        : BaseInstruction{ hw_arch, 0x9D }
+    {}
+
+    //---------------------------------------------------------
+    void JSRDirect::exec()
+    {
+        const memory::Byte value_low_addr{ _hw_arch.load_next_byte() };
+        const memory::MemAddr mem_addr{ _hw_arch.get_directpage_addr(value_low_addr) };
+
+        _hw_arch.push_system_stack(_hw_arch.regPC() & 0xff);
+        _hw_arch.push_system_stack(_hw_arch.regPC() >> 8);
+
+        _hw_arch.regPC = _hw_arch.get_word(mem_addr);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t JSRDirect::get_cycles_count() noexcept
+    {
+        return 7;
+    }
+
+
+    //-----   JSR Indexed   -----------------------------------
+    //---------------------------------------------------------
+    JSRIndexed::JSRIndexed(archi::HWArchitecture& hw_arch) noexcept
+        : BaseInstruction{ hw_arch, 0xAD }
+    {}
+
+    //---------------------------------------------------------
+    void JSRIndexed::exec()
+    {
+        _indexed_mode_ptr = addr::make_indexed_addressing_class(_hw_arch);
+
+        _hw_arch.push_system_stack(_hw_arch.regPC() & 0xff);
+        _hw_arch.push_system_stack(_hw_arch.regPC() >> 8);
+
+        _hw_arch.regPC = _indexed_mode_ptr->get_addressed_word();  // Notice: Not sure of this
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t JSRIndexed::get_cycles_count() noexcept
+    {
+        return 7 + _indexed_mode_ptr->get_byte_cycles();
+    }
+
+
+    //-----   JSR Extended   ----------------------------------
+    //---------------------------------------------------------
+    JSRExtended::JSRExtended(archi::HWArchitecture& hw_arch) noexcept
+        : BaseInstruction{ hw_arch, 0xBD }
+    {}
+
+    //---------------------------------------------------------
+    void JSRExtended::exec()
+    {
+        memory::MemAddr mem_addr{ memory::MemAddr(_hw_arch.load_next_word()) };
+
+        _hw_arch.push_system_stack(_hw_arch.regPC() & 0xff);
+        _hw_arch.push_system_stack(_hw_arch.regPC() >> 8);
+
+        _hw_arch.regPC = _hw_arch.get_word(mem_addr);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t JSRExtended::get_cycles_count() noexcept
+    {
+        return 8;
+    }
+
+}
---
+++ MC6809E/instructions/instrs/jsr.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+
+
+namespace instr
+{
+    //=====   JSR Memory   ====================================
+    //-----   JSR Direct   ------------------------------------
+    struct JSRDirect : public BaseInstruction
+    {
+        JSRDirect(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~JSRDirect() noexcept = default;
+
+        void exec() override;
+        const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   JSR Indexed   -----------------------------------
+    class JSRIndexed : public BaseInstruction
+    {
+    public:
+        JSRIndexed(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~JSRIndexed() noexcept = default;
+
+        void exec() override;
+        const std::uint64_t get_cycles_count() noexcept override;
+
+    private:
+        std::unique_ptr<addr::BaseAddressingMode> _indexed_mode_ptr{ nullptr };
+    };
+
+    //-----   JSR Extended   ----------------------------------
+    struct JSRExtended : public BaseInstruction
+    {
+        JSRExtended(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~JSRExtended() noexcept = default;
+
+        void exec() override;
+        const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+}
