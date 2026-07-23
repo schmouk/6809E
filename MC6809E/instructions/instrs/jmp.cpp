81d753dc0e592c126bde9477f681bc64237d9cff
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-07-23 13:32:04 +0200

    #15-implement the M6809 CPU instructions set
    Added JMP instructions. Builds.

---
+++ MC6809E/instructions/instrs/jmp.cpp
@@ -0,0 +1,79 @@
+#include <cstdint>
+
+#include "./jmp.h"
+
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../addressing_modes/indexed_addressing_factory.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+#include "../../cpu/microproc_unit.h"
+#include "../../cpu/cpu_registers.h"
+
+
+namespace instr
+{
+    //=====   JMP Memory   ====================================
+    //-----   JMP Direct   ------------------------------------
+    //---------------------------------------------------------
+    JMPDirect::JMPDirect(archi::HWArchitecture& hw_arch) noexcept
+        : BaseInstruction{ hw_arch, 0x0E }
+    {}
+
+    //---------------------------------------------------------
+    void JMPDirect::exec()
+    {
+        const memory::Byte value_low_addr{ _hw_arch.load_next_byte() };
+        const memory::MemAddr mem_addr{ _hw_arch.get_directpage_addr(value_low_addr) };
+
+        _hw_arch.regPC = _hw_arch.get_word(mem_addr);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t JMPDirect::get_cycles_count() noexcept
+    {
+        return 3;
+    }
+
+
+    //-----   JMP Indexed   -----------------------------------
+    //---------------------------------------------------------
+    JMPIndexed::JMPIndexed(archi::HWArchitecture& hw_arch) noexcept
+        : BaseInstruction{ hw_arch, 0x6E }
+    {}
+
+    //---------------------------------------------------------
+    void JMPIndexed::exec()
+    {
+        _indexed_mode_ptr = addr::make_indexed_addressing_class(_hw_arch);
+        _hw_arch.regPC = _indexed_mode_ptr->get_addressed_word();  // Notice: Not sure of this
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t JMPIndexed::get_cycles_count() noexcept
+    {
+        return 3 + _indexed_mode_ptr->get_byte_cycles();
+    }
+
+
+    //-----   JMP Extended   ----------------------------------
+    //---------------------------------------------------------
+    JMPExtended::JMPExtended(archi::HWArchitecture& hw_arch) noexcept
+        : BaseInstruction{ hw_arch, 0x7E }
+    {}
+
+    //---------------------------------------------------------
+    void JMPExtended::exec()
+    {
+        memory::MemAddr mem_addr{ memory::MemAddr(_hw_arch.load_next_word()) };
+        _hw_arch.regPC = _hw_arch.get_word(mem_addr);
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t JMPExtended::get_cycles_count() noexcept
+    {
+        return 4;
+    }
+
+}
---
+++ MC6809E/instructions/instrs/jmp.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include "../base_instruction.h"
+
+#include "../../addressing_modes/base_addressing.h"
+#include "../../architecture/hw_architecture.h"
+#include "../../memory/types.h"
+
+
+namespace instr
+{
+    //=====   JMP Memory   ====================================
+    //-----   JMP Direct   ------------------------------------
+    struct JMPDirect : public BaseInstruction
+    {
+        JMPDirect(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~JMPDirect() noexcept = default;
+
+        void exec() override;
+        const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+    //-----   JMP Indexed   -----------------------------------
+    class JMPIndexed : public BaseInstruction
+    {
+    public:
+        JMPIndexed(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~JMPIndexed() noexcept = default;
+
+        void exec() override;
+        const std::uint64_t get_cycles_count() noexcept override;
+
+    private:
+        std::unique_ptr<addr::BaseAddressingMode> _indexed_mode_ptr{ nullptr };
+    };
+
+    //-----   JMP Extended   ----------------------------------
+    struct JMPExtended : public BaseInstruction
+    {
+        JMPExtended(archi::HWArchitecture& hw_arch) noexcept;
+        virtual ~JMPExtended() noexcept = default;
+
+        void exec() override;
+        const std::uint64_t get_cycles_count() noexcept override;
+    };
+
+}
