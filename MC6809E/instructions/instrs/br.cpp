bb1cfda188589b27172d32aa0b881379c005d2ac
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-07-31 21:56:58 +0200

    #15-implement the M6809 CPU instructions set
    Added few branching instructions. Builds

--- MC6809E/instructions/instrs/br.cpp
+++ MC6809E/instructions/instrs/br.cpp
@@ -65,7 +65,7 @@ namespace instr
             _hw_arch.regPC += get_offset();  // Notice: evaluated at LongRelativeAddressing construction time
         }
 
-        return get_cycles_count() + branch_taken;  // Notice: similar to (branch_taken ? 1 : 0);
+        return get_cycles_count() + branch_taken;  // Notice: similar to " + (branch_taken ? 1 : 0);"
     }
 
     //---------------------------------------------------------
@@ -120,6 +120,170 @@ namespace instr
     const std::uint64_t LBCS::exec()
     {
         return _exec(_hw_arch.regCC.carry_flag());
+    }
+
+
+    //=====   BEQ / LBEQ   ====================================
+    //---------------------------------------------------------
+    BEQ::BEQ(archi::HWArchitecture& hw_arch)
+        : ShortRelativeBranchingBase(hw_arch, 0x27)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t BEQ::exec()
+    {
+        return _exec(_hw_arch.regCC.zero_flag());
+    }
+
+    //---------------------------------------------------------
+    LBEQ::LBEQ(archi::HWArchitecture& hw_arch)
+        : LongRelativeBranchingBase(hw_arch, 0x10, 0x27)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t LBEQ::exec()
+    {
+        return _exec(_hw_arch.regCC.zero_flag());
+    }
+
+
+    //=====   BGE / LBGE   ====================================
+    //---------------------------------------------------------
+    BGE::BGE(archi::HWArchitecture& hw_arch)
+        : ShortRelativeBranchingBase(hw_arch, 0x2C)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t BGE::exec()
+    {
+        return _exec(!(_hw_arch.regCC.negative_flag() ^ _hw_arch.regCC.overflow_flag()));
+    }
+
+    //---------------------------------------------------------
+    LBGE::LBGE(archi::HWArchitecture& hw_arch)
+        : LongRelativeBranchingBase(hw_arch, 0x10, 0x2C)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t LBGE::exec()
+    {
+        return _exec(!(_hw_arch.regCC.negative_flag() ^ _hw_arch.regCC.overflow_flag()));
+    }
+
+
+    //=====   BGT / LBGT   ====================================
+    //---------------------------------------------------------
+    BGT::BGT(archi::HWArchitecture& hw_arch)
+        : ShortRelativeBranchingBase(hw_arch, 0x2E)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t BGT::exec()
+    {
+        return _exec(
+            !_hw_arch.regCC.zero_flag() &&
+            !(_hw_arch.regCC.negative_flag() ^ _hw_arch.regCC.overflow_flag())
+        );
+    }
+
+    //---------------------------------------------------------
+    LBGT::LBGT(archi::HWArchitecture& hw_arch)
+        : LongRelativeBranchingBase(hw_arch, 0x10, 0x2E)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t LBGT::exec()
+    {
+        return _exec(
+            !_hw_arch.regCC.zero_flag() &&
+            !(_hw_arch.regCC.negative_flag() ^ _hw_arch.regCC.overflow_flag())
+        );
+    }
+
+
+    //=====   BHI / LBHI   ====================================
+    //---------------------------------------------------------
+    BHI::BHI(archi::HWArchitecture& hw_arch)
+        : ShortRelativeBranchingBase(hw_arch, 0x22)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t BHI::exec()
+    {
+        return _exec(!_hw_arch.regCC.zero_flag() && !_hw_arch.regCC.negative_flag());
+    }
+
+    //---------------------------------------------------------
+    LBHI::LBHI(archi::HWArchitecture& hw_arch)
+        : LongRelativeBranchingBase(hw_arch, 0x10, 0x22)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t LBHI::exec()
+    {
+        return _exec(!_hw_arch.regCC.zero_flag() && !_hw_arch.regCC.negative_flag());
+    }
+
+
+    //=====   BLE / LBLE   ====================================
+    //---------------------------------------------------------
+    BLE::BLE(archi::HWArchitecture& hw_arch)
+        : ShortRelativeBranchingBase(hw_arch, 0x2F)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t BLE::exec()
+    {
+        return _exec(
+            _hw_arch.regCC.zero_flag() ||
+            (_hw_arch.regCC.negative_flag() ^ _hw_arch.regCC.overflow_flag())
+        );
+    }
+
+    //---------------------------------------------------------
+    LBLE::LBLE(archi::HWArchitecture& hw_arch)
+        : LongRelativeBranchingBase(hw_arch, 0x10, 0x2F)
+    {}
+
+    //---------------------------------------------------------
+    const std::uint64_t LBLE::exec()
+    {
+        return _exec(
+            _hw_arch.regCC.zero_flag() ||
+            (_hw_arch.regCC.negative_flag() ^ _hw_arch.regCC.overflow_flag())
+        );
+    }
+
+
+
+
+
+
+
+
+    //=====   BNE / LBNE   ====================================
+    //---------------------------------------------------------
+    BNE::BNE(archi::HWArchitecture& hw_arch)
+        : ShortRelativeBranchingBase(hw_arch, 0x27)
+    {
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t BNE::exec()
+    {
+        return _exec(!_hw_arch.regCC.zero_flag());
+    }
+
+    //---------------------------------------------------------
+    LBNE::LBNE(archi::HWArchitecture& hw_arch)
+        : LongRelativeBranchingBase(hw_arch, 0x10, 0x27)
+    {
+    }
+
+    //---------------------------------------------------------
+    const std::uint64_t LBNE::exec()
+    {
+        return _exec(!_hw_arch.regCC.zero_flag());
     }
 
 }
--- MC6809E/instructions/instrs/br.h
+++ MC6809E/instructions/instrs/br.h
@@ -76,4 +76,139 @@ namespace instr
         const std::uint64_t exec() override;
     };
 
+
+    //=====   BEQ / LBEQ   ====================================
+    //---------------------------------------------------------
+    struct BEQ : public ShortRelativeBranchingBase
+    {
+        BEQ(archi::HWArchitecture& hw_arch);
+        virtual ~BEQ() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+    //---------------------------------------------------------
+    struct LBEQ : public LongRelativeBranchingBase
+    {
+        LBEQ(archi::HWArchitecture& hw_arch);
+        virtual ~LBEQ() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+
+    //=====   BGE / LBGE   ====================================
+    // Notice: signed comparisons
+    //---------------------------------------------------------
+    struct BGE : public ShortRelativeBranchingBase
+    {
+        BGE(archi::HWArchitecture& hw_arch);
+        virtual ~BGE() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+    //---------------------------------------------------------
+    struct LBGE : public LongRelativeBranchingBase
+    {
+        LBGE(archi::HWArchitecture& hw_arch);
+        virtual ~LBGE() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+
+    //=====   BGT / LBGT   ====================================
+    // Notice: signed comparisons
+    //---------------------------------------------------------
+    struct BGT : public ShortRelativeBranchingBase
+    {
+        BGT(archi::HWArchitecture& hw_arch);
+        virtual ~BGT() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+    //---------------------------------------------------------
+    struct LBGT : public LongRelativeBranchingBase
+    {
+        LBGT(archi::HWArchitecture& hw_arch);
+        virtual ~LBGT() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+
+    //=====   BHI / LBHI   ====================================
+    // Notice: unsigned comparisons
+    //---------------------------------------------------------
+    struct BHI : public ShortRelativeBranchingBase
+    {
+        BHI(archi::HWArchitecture& hw_arch);
+        virtual ~BHI() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+    //---------------------------------------------------------
+    struct LBHI : public LongRelativeBranchingBase
+    {
+        LBHI(archi::HWArchitecture& hw_arch);
+        virtual ~LBHI() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+
+    //=====   BHS / LBHS   ====================================
+    // Notice: unsigned comparisons
+    using BHS  = BCC;
+    using LBHS = LBCC;
+
+
+    //=====   BLE / LBLE   ====================================
+    // Notice: signed comparisons
+    //---------------------------------------------------------
+    struct BLE : public ShortRelativeBranchingBase
+    {
+        BLE(archi::HWArchitecture& hw_arch);
+        virtual ~BLE() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+    //---------------------------------------------------------
+    struct LBLE : public LongRelativeBranchingBase
+    {
+        LBLE(archi::HWArchitecture& hw_arch);
+        virtual ~LBLE() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+
+
+
+
+
+
+    //=====   BNE / LBNE   ====================================
+    //---------------------------------------------------------
+    struct BNE : public ShortRelativeBranchingBase
+    {
+        BNE(archi::HWArchitecture& hw_arch);
+        virtual ~BNE() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
+    //---------------------------------------------------------
+    struct LBNE : public LongRelativeBranchingBase
+    {
+        LBNE(archi::HWArchitecture& hw_arch);
+        virtual ~LBNE() noexcept = default;
+
+        const std::uint64_t exec() override;
+    };
+
 }
